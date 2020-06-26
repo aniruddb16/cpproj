26f4d4c084ed8c6954fa8cb29b78ff5a23409b2a
icecreamcake101 <38510401+icecreamcake101@users.noreply.github.com>
2020-06-26 18:54:43 +0530

    Update proj.c

--- proj.c
+++ proj.c
@@ -2,14 +2,52 @@
 #include <stdlib.h>
 
 //globals
+void openscreen()
+{
+printf("*******************WELCOME TO KAUN BANEGA CROREPATI************************* \n \n");
+printf("**************The total number of questions are 10************************** \n \n");
+printf("***************Intermediate question levels are 3 and 7********************* \n \n");
+printf("***************Prize money for each question is Rs. 10,00,000***************\n \n");
+printf("Lifelines available are \n 50-50: Two incorrect options will be eliminated and \n Flip the Question: A new question will be displayed to you. \n \n");
+printf("DO YOU HAVE WHAT IT TAKES TO BE A CROREPATI??? \n \n Press any key to begin \n \n");
+getchar();
+system("clear");
+}
 
-char question_pool[10][200]={"first question \n(a)option a \t\t\t (b)option b\n(c)option c \t\t\t (d)option d\n","q2","q3","q4","q5","q6","q7","q8","q9","q10"};
-char question_pool_fiftyfifty[10][200]={"q1f","q2f","q3f","q4f","q5f","q6f","q7f","q8f","q9f","q10f"};
-char answer_pool[10]={'a','b','c','d','d','c','b','a','a','b'};
-char question_flipquestion[1][200]={"bq"};
-char question_flipquestion_fiftyfifty[1][200]={"bqf"};
+char question_pool[10][200]={"Q1. Which is the world's most valuable currency in terms of exchange rate? \nA.	United States Dollar \t\t\tB.	Bahraini Dinar\nC.	Great Britain Pound \t\t\tD.	Kuwaiti Dinar\n",
+"Q2. What is the national bird of the United States of America? \n A.	Peregrine falcon\t\t\tB.	Bald eagle \nC.	King vulture \t\t\tD.	Golden eagle \n",
+"Q3. Which dog breed's name in Chinese translates to 'puffy lion dog' in English? \nA.	Lhasa Apso \t\t\tB.	Chow-Chow\nC.	Xiasi \t\t\tD.	Shih Tzu \n",
+"Q4. Which Indian state suffered an oil spill which lead to wide spread destruction of wildlife in May 2020? \nA.	Mizoram \t\t\tB.	Meghalaya \nC.	Assam \t\t\tD.	Nagaland \n",
+"Q5. What is the name of the facial recognition technology developed by Russia for monitoring school children? \nA.	Orwell \t\t\tB.	Simon \nC.	Watcher \t\t\tD.	Big Brother \n",
+"Q6. Which of the following countries has not reported any positive coronavirus cases? \nA.	North Korea \t\t\tB.	Mongolia \nC.	Bosnia & Herzegovina \t\t\tD.	Azerbaijan \n",
+"Q7. What is the fourth derivative of displacement with respect to time? \nA.	Pop \t\t\tB.	Crack \nC.	Snap \t\t\tD.	Jerk \n",
+"Q8. Which of the following sports will NOT be making its debut in the Tokyo Olympics? \nA.	Skateboarding \t\t\tB.	Karate \nC.	Surfing \t\t\tD.	Baseball \n",
+"Q9. Who is the governor of the Reserve Bank of India as of June 2020? \nA.	Urjit Patel \t\t\tB.	Shantikanta Das \nC.	Gita Gopinath \t\t\tD.	Bimal Jalan \n",
+"Q10. Which of the following is an Abrahamic religion? \nA.	Rastafarianism \t\t\tB.	Zoroastrianism \nC.	Shintoism \t\t\tD.	Hinduism \n"};
+
+//50-50 options
+char question_pool_fiftyfifty[10][200]={"Q1. Which is the world's most valuable currency in terms of exchange rate? \nA.	 \t\t\tB.	Bahraini Dinar\nC.	 \t\t\tD.	Kuwaiti Dinar\n",
+"Q2. What is the national bird of the United States of America? \n A.	\t\t\tB.	Bald eagle \nC.	 \t\t\tD.	Golden eagle \n",
+"Q3. Which dog breed's name in Chinese translates to 'puffy lion dog' in English? \nA.	 \t\t\tB.	Chow-Chow\nC.	Xiasi \t\t\tD.	 \n",
+"Q4. Which Indian state suffered an oil spill which lead to wide spread destruction of wildlife in May 2020? \nA.	Mizoram \t\t\tB.	 \nC.	Assam \t\t\tD.	 \nf",
+"Q5. What is the name of the facial recognition technology developed by Russia for monitoring school children? \nA.	Orwell \t\t\tB.	 \nC.	Watcher \t\t\tD.	 \n",
+"Q6. Which of the following countries has not reported any positive coronavirus cases? \nA.	North Korea \t\t\tB.	 \nC.	Bosnia & Herzegovina \t\t\tD.	 \n",
+"Q7. What is the fourth derivative of displacement with respect to time? \nA.	 \t\t\tB.	Crack \nC.	Snap \t\t\tD.	 \n",
+"Q8. Which of the following sports will NOT be making its debut in the Tokyo Olympics? \nA.	 \t\t\tB.	Karate \nC.	 \t\t\tD.	Baseball \n",
+"Q9. Who is the governor of the Reserve Bank of India as of June 2020? \nA.	 \t\t\tB.	Shantikanta Das \nC.	 \t\t\tD.	Bimal Jalan \n",
+"Q10. Which of the following is an Abrahamic religion? \nA.	Rastafarianism \t\t\tB.	Zoroastrianism \nC.	 \t\t\tD.	 \n"};
+
+//answer key
+char answer_pool[10]={'d','b','b','c','a','a','c','d','b','a'};
+
+//flipped question
+char question_flipquestion[1][200]={"Which of the following elements is named after the Scandinavian goddess of beauty and fertility? \nA.	Rhenium \t\t\tB.	Selenium \nC.	Vanadium \t\t\tD.	Erbium \n"};
+//flipped question with 50-50 options
+char question_flipquestion_fiftyfifty[1][200]={"bqfWhich of the following elements is named after the Scandinavian goddess of beauty and fertility? \nA.	 \t\t\tB.	Selenium \nC.	Vanadium \t\t\tD.	 \n"};
+//answer to flipped question
 char answer_flipquestion='c';
-int prize_money_pool[10]={100,200,300,400,500,600,700,800,900,1000};
+
+int prize_money_pool[10]={1000000,2000000,3000000,4000000,5000000,6000000,7000000,8000000,9000000,10000000};
 int prize_money=0;
 int flag_wrong_answer=0;
 char option;
@@ -27,11 +65,11 @@ void reset_globals()
   lifeline_fiftyfifty=0;
   lifeline_flipquestion=0;
   lifelines_left=2;
-  exit_status=0;
 }
 void winscreen()
 {
-  printf("congratulations! Your answer_poolwer is correct! \n");
+  printf("congratulations! Your answer is correct! \n");
+
 	printf("your prize money is %d \n",prize_money);
   if(lifeline_fiftyfifty==0 &&lifeline_flipquestion==0)
     lifelines_left=2;
@@ -42,24 +80,38 @@ void winscreen()
   else
     lifelines_left=0;
 	printf("lifelines left: %d \n",lifelines_left);
-	printf("question_pooltions left: %d\n",q);
+	printf("questions left: %d\n",questions_left);
   getchar();
   system("clear");
 }
+
+void exit_screen()
+{
+  printf("your prize money is %d \n",prize_money);
+  exit_status=1;
+  printf("Press any key to continue\n");
+  getchar();
+  system("clear");
+}
+
 
 
 void gamewon()
 {
 	printf("congratulations! You have won the game! \n");
 	printf("your prize money is %d \n",prize_money);
+  printf("Press any key to continue\n");
 }
 
 
 void lossscreen()
 {
 	char response;
-
-	printf("That was the wrong answer_poolwer. \n \n Unfortunately this is the end of your game. \n \n Your prize money is Rs. %d \n \n", prize_money);
+  if (prize_money<4000000)
+   prize_money=0;
+  else
+   prize_money=4000000;
+	printf("That was the wrong answer. \n \n Unfortunately this is the end of your game. \n \n Your prize money is Rs. %d \n \n", prize_money);
   printf("Press Q to quit or T to try again \n");
 	scanf("%c", &response);
   getchar();
@@ -69,11 +121,12 @@ void lossscreen()
 	}
 	else if(response=='T'||response=='t')
 	{
-        exit_status=0;
+        exit_status=1;
 	}
 	else
 	{
-    printf("iNVALID RESPONSE");
+    printf("INVALID RESPONSE");
+    printf("Press any key to continue\n");
     getchar();
     system("clear");
     lossscreen();}
@@ -82,7 +135,7 @@ void lossscreen()
 
 
 int main()
-{
+{ openscreen();
   do{
   do {
     for ( i=0;i<=10;i++)
@@ -95,7 +148,7 @@ int main()
         exit(0);
       }
       puts(question_pool[i]);
-      printf("\nEnter option : ");
+      printf("\nEnter option( 'e' for fifty fifty, 'f' for flip the question, 'g' to exit with money) : ");
       scanf("%c",&option);
       getchar();
       printf("\n");
@@ -104,7 +157,7 @@ int main()
       {
         system("clear");
         puts(question_pool_fiftyfifty[i]);
-        printf("\nEnter option : ");
+        printf("\nEnter option( 'e' for fifty fifty, 'f' for flip the question, 'g' to exit with money) : ");
         scanf("%c",&option);
         getchar();
         printf("\n");
@@ -114,7 +167,7 @@ int main()
           { lifeline_flipquestion=1;
             system("clear");
             puts(question_flipquestion);
-            printf("\nEnter option : ");
+            printf("\nEnter option( 'e' for fifty fifty, 'f' for flip the question, 'g' to exit with money) : ");
             scanf("%c",&option);
             getchar();
             printf("\n");
@@ -147,7 +200,7 @@ int main()
       { lifeline_flipquestion=1;
         system("clear");
         puts(question_flipquestion);
-        printf("\nEnter option : ");
+        printf("\nEnter option( 'e' for fifty fifty, 'f' for flip the question, 'g' to exit with money) : ");
         scanf("%c",&option);
         getchar();
         printf("\n");
@@ -157,7 +210,7 @@ int main()
         { lifeline_fiftyfifty=1;
           system("clear");
           puts(question_flipquestion_fiftyfifty);
-          printf("\nEnter option : ");
+          printf("\nEnter option( 'e' for fifty fifty, 'f' for flip the question, 'g' to exit with money) : ");
           scanf("%c",&option);
           getchar();
           printf("\n");
@@ -194,6 +247,13 @@ int main()
         winscreen();
       }
 
+      else if(option=='g')
+      {
+        exit_screen();
+        exit(0);
+
+      }
+
       else
       {
         lossscreen();
@@ -202,13 +262,14 @@ int main()
       }
 
 
+
     }system("clear");
   } while(flag_wrong_answer!=1);
 
 reset_globals();
 }while(exit_status!=1);
 
-getchar();
+
 system("clear");
 
 
